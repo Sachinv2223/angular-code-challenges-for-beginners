63cefdb63e170c302e9e150d2d14bd56eeb1bd64
Sachin Varghese <sachinv2223@gmail.com>
2025-09-27 23:04:24 +0530

    project init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {Challenge1Component} from './challenges/challenge1-component/challenge1-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/challenge1', pathMatch: 'full'},
+  { path: 'challenge1', component: Challenge1Component }
+];
